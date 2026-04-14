dd4d3680e0a2cae574e542ba2e8098d4e7ac8a1a
shivankvirdi <shivank.virdi@gmail.com>
2026-04-13 21:15:08 -0700

    Update vision_model.py

--- vision_model.py
+++ vision_model.py
@@ -3,7 +3,6 @@ import clip
 import torch
 import requests
 from io import BytesIO
-import pytesseract
 from PIL import Image, ImageGrab
 import matplotlib.pyplot as plt
 
@@ -16,15 +15,6 @@ categories=[
 
 def load_model(model_name='ViT-B/32'):
     return clip.load(model_name, device=device)
-
-def load_ocr_path():
-    TESSERACT_PATH = r"C:\Users\shiva\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
-
-    if not os.path.isfile(TESSERACT_PATH):
-        print("ERROR: tesseract.exe not found at:")
-        print(TESSERACT_PATH)
-
-    pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
 
 def load_image(src: str):
     try:
@@ -50,12 +40,7 @@ def classify(image, model, preprocess, categories):
 
     return {'label': categories[t_index[0].item()], 'val': t_value[0].item()}
 
-def ocr_cap(img):
-    text = pytesseract.image_to_string(img)
-    print(f"Model: {text}")
-
 def run_model():
-    load_ocr_path()
     model, preprocess = load_model()
     # you can input any image type consistent with the categories
     image = load_image('https://www.sellerapp.com/blog/wp-content/uploads/2022/05/amazon-product-detail-page-standards-1100x489.jpg')
@@ -66,7 +51,6 @@ def run_model():
     plt.show()
 
     print(f"Category: {result['label']:>16s}: {100 * result['val']:.2f}% match")
-    ocr_cap(image)
 
 if __name__ == '__main__':
     run_model()
